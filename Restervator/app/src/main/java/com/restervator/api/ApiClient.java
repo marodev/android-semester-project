f69f593ac98f719c151648eba7b73a016e22d8df
marodev <modermatt@tuta.io>
2019-12-08 11:42:45 +0100

    add api client, change to Java 8

--- Restervator/app/src/main/java/com/restervator/api/ApiClient.java
+++ Restervator/app/src/main/java/com/restervator/api/ApiClient.java
@@ -1,21 +1,52 @@
 package com.restervator.api;
 
+
+import com.restervator.BuildConfig;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    private static final String BASE_URL = "https://developers.zomato.com";
 
     public static Retrofit getApiClient() {
         if (retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(provideOkHttpClient())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return retrofit;
+    }
+
+    private static OkHttpClient provideOkHttpClient() {
+        return new OkHttpClient.Builder()
+                .addInterceptor(provideInterceptor())
+                .addInterceptor(provideHttpLoggingInterceptor())
+                .build();
+    }
+
+    private static Interceptor provideInterceptor() {
+        return chain -> {
+            Request modifiedRequest = chain.request().newBuilder()
+                    .addHeader("user-key", BuildConfig.apikey)
+                    .build();
+
+            return chain.proceed(modifiedRequest);
+        };
+    }
+
+    private static HttpLoggingInterceptor provideHttpLoggingInterceptor() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        return loggingInterceptor;
     }
 }
