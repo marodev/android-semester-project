ea85c5fe25134cfee5cb5f1087377f5559fabc36
marodev <modermatt@tuta.io>
2019-12-11 15:54:22 +0100

    init reservation overview

---
+++ Restervator/app/src/main/java/com/restervator/ReservationOverviewActivity.java
@@ -0,0 +1,14 @@
+package com.restervator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReservationOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_overview);
+    }
+}
